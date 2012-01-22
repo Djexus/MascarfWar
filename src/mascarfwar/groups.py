06fce82f9e631073ecf8f8bbcc1fa1f2597dc5af
Marko Nervo <marko@python.it>
2012-01-22 23:18:58 +0100

    Removed add, move & attack methods from Field

--- src/mascarfwar/groups.py
+++ src/mascarfwar/groups.py
@@ -50,18 +50,3 @@ class Field(Group):
         self.cellsize = cellsize
         self.coords = [(x, y) for x in range(mapsize[0]) for y in range(mapsize[1])]
         super(Field, self).__init__()
-
-    def add(self, unit):
-
-        if unit['position'] in self.coords:
-            super(Field, self).add(unit)
-
-    def move_unit(self, unit, position):
-
-        if (position in self.coords) and unit.can_move(self, position):
-            pass
-
-    def attack_unit(self, unit, enemy):
-
-        if (enemy in self) and unit.can_attack(self, enemy):
-            pass
